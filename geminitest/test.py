4731aac9300856b2868e4068edb81841c0e872fa
manit2004 <manit.roy@research.iiit.ac.in>
2025-10-12 04:13:34 +0530

    fix(damage-det): correct inference notebook configuration

--- geminitest/test.py
+++ geminitest/test.py
@@ -11,14 +11,4 @@ result = detector.verify_image_description(
     "dent on the rear bumper of the car"        # Change this
 )
 
-# Print formatted output with newlines
-print(result['formatted_output'])
-
-# Print damage vector
-print("\nDAMAGE VECTOR:")
-print(result['damage_vector'])
-
-# If you want to see the raw dictionary, uncomment below:
-# import json
-# print("\n\nRAW RESULT:")
-# print(json.dumps(result, indent=2))
+print(result)
